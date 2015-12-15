35de18524bcad22a2b4005b5eb90e05ebc9a4756
Jerod Mills <jerod.mills@gmail.com>
2015-12-15 13:00:15 -0500

    fixed definition

--- smartapps/superuser/smoke-detector.src/smoke-detector.groovy
+++ smartapps/superuser/smoke-detector.src/smoke-detector.groovy
@@ -5,6 +5,18 @@
  *  Date: 2013-10-14
  */
 
+
+// Automatically generated. Make future change here.
+definition(
+    name: "Smoke Detector",
+    namespace: "",
+    author: "jerod.mills@gmail.com",
+    description: "Smoke Detector",
+    category: "My Apps",
+    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
+    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience%402x.png",
+    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience%402x.png")
+
 preferences {
 	section("Smoke Detector..."){
 		input "smoke", "capability.smokeDetector", title: "Which?"
